3e23f132c697249736f1d435b57bf5cecd73c186
Shota Chakhvashvili <shota4ax@mail.ru>
2016-09-20 17:03:36 +0300

    Update PORT to heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express();
 
-var PORT = 3000;
+var PORT = process.env.PORT||3000;
 //request what user send (json, cookie, data) and response what we send to user
 
 var middleware = require('./middleware.js');
@@ -21,3 +21,4 @@ app.use(express.static(__dirname+'/public'));//use html file
 app.listen(PORT,function(){
     console.log('Express server started at port:'+PORT);
 });//порт который не используется компьютером
+
